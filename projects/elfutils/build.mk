5e42f2bfc58b8fc8c293459144973badd2dbdc37
Michal Gregorczyk <michalgr@live.com>
2022-02-20 22:34:41 +0100

    bump libelf version

--- projects/elfutils/build.mk
+++ projects/elfutils/build.mk
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-ELFUTILS_ANDROID_DEPS = argp
+ELFUTILS_ANDROID_DEPS = argp obstack
 $(eval $(call project-define,elfutils))
 
 ELFUTILS_EXTRA_CFLAGS += -I$(abspath projects/elfutils/android_fixups)
@@ -14,9 +14,11 @@ $(ELFUTILS_ANDROID):
 $(ANDROID_BUILD_DIR)/elfutils: $(ANDROID_CONFIG_SITE)
 	-mkdir $@
 	cd $@ && EXTRA_CFLAGS="$(ELFUTILS_EXTRA_CFLAGS)" $(ELFUTILS_SRCS)/configure \
-		$(ANDROID_EXTRA_CONFIGURE_FLAGS)
+		$(ANDROID_EXTRA_CONFIGURE_FLAGS) \
+		--disable-debuginfod \
+		--disable-libdebuginfod
 
-ELFUTILS_VERSION = 0.176
+ELFUTILS_VERSION = 0.186
 ELFUTILS_URL = http://sourceware.org/pub/elfutils/$(ELFUTILS_VERSION)/elfutils-$(ELFUTILS_VERSION).tar.bz2
 projects/elfutils/sources: | $(DOWNLOADS_DIR)
 	wget $(ELFUTILS_URL) -O $(DOWNLOADS_DIR)/elfutils-$(ELFUTILS_VERSION).tar.bz2
