a832a7eeaa3384b68c3f0fbacde139892935c286
Michal Gregorczyk <michalgr@live.com>
2022-08-29 16:24:28 +0200

    build python for host

--- projects/python/build.mk
+++ projects/python/build.mk
@@ -1,6 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 PYTHON_ANDROID_DEPS = ffi
+PYTHON_HOST_DEPS = ffi
 $(eval $(call project-define,python))
 
 PYTHON_EXTRA_CONFIG_OPTIONS = --build=x86_64 --disable-ipv6 --without-ensurepip --with-system-ffi
@@ -12,12 +13,22 @@ $(PYTHON_ANDROID):
 	cp $(PYTHON_SRCS)/LICENSE $(ANDROID_OUT_DIR)/licenses/python
 	touch $@
 
+$(PYTHON_HOST):
+	cd $(PYTHON_HOST_BUILD_DIR) && make install -j $(THREADS)
+	touch $@
+
 $(PYTHON_ANDROID_BUILD_DIR): \
     export PKG_CONFIG_LIBDIR=$(abspath $(ANDROID_OUT_DIR)/lib/pkgconfig)
 $(PYTHON_ANDROID_BUILD_DIR): $(ANDROID_CONFIG_SITE)
 	mkdir -p $@
 	cd $@ && $(PYTHON_SRCS)/configure \
 		$(ANDROID_EXTRA_CONFIGURE_FLAGS) \
+		$(PYTHON_EXTRA_CONFIG_OPTIONS)
+
+$(PYTHON_HOST_BUILD_DIR): $(HOST_CONFIG_SITE)
+	mkdir -p $@
+	cd $@ && $(PYTHON_SRCS)/configure \
+		$(HOST_EXTRA_CONFIGURE_FLAGS) \
 		$(PYTHON_EXTRA_CONFIG_OPTIONS)
 
 PYTHON_BRANCH_OR_TAG = v3.10.6
