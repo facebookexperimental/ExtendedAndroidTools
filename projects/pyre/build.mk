e08e0cd2aa3f08a70ae9d3749c38c9eb2997ec2a
Michael A. Wekesa <Michael A. Wekesa>
2023-10-04 22:20:08 +0300

    update makefile

--- projects/pyre/build.mk
+++ projects/pyre/build.mk
@@ -1,6 +1,9 @@
-PYRE_HOST_DEPS = python
-$(eval $(call project-define,pyre))
+PYRE_CHECK_DONE := build/host/pyre-check.done
 
+pyre-host: $(PYRE_CHECK_DONE)
 
-projects/pyre/sources:
+$(PYRE_CHECK_DONE): python-host
 	$(HOST_OUT_DIR)/bin/python3 -m pip install pyre-check
+	mkdir -p build/host
+	touch $@
+
