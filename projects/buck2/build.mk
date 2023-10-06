f2e9900e62c26d25b2556a9eba0957025703d569
Daquiver1 <Cabrokwa11@gmail.com>
2023-10-06 12:33:12 +0000

    fix: Replace unzip with zstd and remove unneccessary variables.

--- projects/buck2/build.mk
+++ projects/buck2/build.mk
@@ -1,50 +1,16 @@
-# --------------------------------------------------------------------------------
-# Project: Buck2 Build Script
-# Description: This script is used for building and managing the Buck2 project.
-# Author: Christian Abrokwa
-# --------------------------------------------------------------------------------
+BUCK2_VERSION := 2023-09-01
+BUCK2_ARCHIVE := buck2.zst
+BUCK2_URL := https://github.com/facebook/buck2/releases/download/$(BUCK2_VERSION)/buck2-x86_64-unknown-linux-gnu.zst
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+$(HOST_OUT_DIR)/bin/buck2: $(DOWNLOADS_DIR)/$(BUCK2_ARCHIVE) | $(HOST_OUT_DIR)
+# commands to unpack $(BUCK2_ARCHIVE) and set the executable flag
+	zstd -d $(DOWNLOADS_DIR)/$(BUCK2_ARCHIVE) -o $(HOST_OUT_DIR)/bin/buck2
+	chmod +x $(HOST_OUT_DIR)/bin/buck2
 
-$(eval $(call project-define,buck2))
-
-PROJECT_NAME := buck2
-PROJECT_VERSION := 2023-09-01
-
-HOST_OUT_DIR := out/host/bin
-BUILD_HOST_DIR :=build/host
-
-BUCK2_HOST_OUT_TARGET := $(HOST_OUT_DIR)/buck2
-BUCK2_HOST_BUILD_TARGET := $(BUILD_HOST_DIR)
-BUCK2_ZIP_TARGET := $(BUCK2_OUT_TARGET).zip
-
-BUCK2_URL := https://github.com/facebook/$(PROJECT_NAME)/archive/refs/tags/$(PROJECT_VERSION).zip
-
-# ZSTD := $(shell command -v zstd 2> /dev/null)
-# ifeq ($(ZSTD),)
-# 	ZSTD := $(shell apt-get -qy install zstd && command -v zstd)
-# 	ifeq ($(ZSTD),)
-# 		$(error "Failed to install zstd. Please install it manually and try again.")
-# 	endif
-# endif
-
-
-# Define the target for the host build
-$(BUCK2_HOST): $(BUCK2_HOST_BUILD_DIR)
-	@echo "Buck2 host build complete"
-
-$(BUCK2_HOST_BUILD_DIR):
-	mkdir -p $(BUCK2_HOST_OUT_TARGET) $(BUCK2_HOST_BUILD_TARGET)
-	wget -q -O  $(BUCK2_ZIP_TARGET) $(BUCK2_URL)
-	unzip $(BUCK2_ZIP_TARGET) -d $(BUCK2_HOST_OUT_TARGET)
-	cp -r $(BUCK2_HOST_OUT_TARGET)/* $(BUCK2_HOST_BUILD_TARGET)
-	chmod +x $(BUCK2_HOST_OUT_TARGET)/*
-	rm $(BUCK2_ZIP_TARGET)
-	touch $@
+$(DOWNLOADS_DIR)/$(BUCK2_ARCHIVE): | $(DOWNLOADS_DIR)
+# instructions to download the archive
+	wget -q -O $(DOWNLOADS_DIR)/$(BUCK2_ARCHIVE) $(BUCK2_URL)
 
 # Phony target for host
 .PHONY: buck2-host
-buck2-host: $(BUCK2_HOST)
-
-projects/buck2/sources: $(BUCK2_HOST)
-#	git clone $(BUCK2_REPO) $@ --depth=1 -b $(BUCK2_BRANCH_OR_TAG)
+buck2-host: $(HOST_OUT_DIR)/bin/buck2
