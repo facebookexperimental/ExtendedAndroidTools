cd2016a2ec6d456e1d466d1e85bb154a65ec30cd
Michal Gregorczyk <michalgr@live.com>
2023-10-25 12:55:19 +0200

    Factor out 'pip install' calls

--- projects/pyre/build.mk
+++ projects/pyre/build.mk
@@ -1,10 +1,3 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
-PYRE_CHECK_DONE := build/host/pyre-check.done
-
-pyre-host: $(PYRE_CHECK_DONE)
-
-$(PYRE_CHECK_DONE): python-host
-	$(HOST_OUT_DIR)/bin/python3 -m pip install pyre-check
-	mkdir -p build/host
-	touch $@
+$(eval $(call pip-project,pyre,pyre-check))
