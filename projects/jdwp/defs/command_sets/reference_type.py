79be56a4fbb98567092996e197bbefe6fa74ecb4
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-10-25 07:52:50 +0000

    Updated error field

--- projects/jdwp/defs/command_sets/reference_type.py
+++ projects/jdwp/defs/command_sets/reference_type.py
@@ -1,48 +1,40 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
 """Command Set: ReferenceType."""
 
-from jdwp.defs.schema import CommandSet
-from jdwp.defs.commands.reference_type import (
-    Signature,
-    ClassLoader,
-    Modifiers,
-    Fields,
-    Methods,
-    GetValues,
-    SourceFile,
-    NestedTypes,
-    Status,
-    Interfaces,
-    ClassObject,
-    SourceDebugExtension,
-    SignatureWithGeneric,
-    FieldsWithGeneric,
-    MethodsWithGeneric,
-    Instances,
-    ClassFileVersion,
-    ConstantPool,
+from jdwp.defs.schema import CommandSet, Type
+from jdwp.defs.schema import Command, Field, Struct
+from projects.jdwp.defs.constants import ErrorType
+from collections.abc import Sequence
+
+
+Signature = Command(
+    name="Signature",
+    id=1,
+    out=Struct(
+        [
+            Field("refType", Type.REFERENCE_TYPE_ID, "The Reference type ID."),
+        ]
+    ),
+    reply=Struct(
+        [
+            Field(
+                "signature", Type.STRING, "The JNI signature for the reference type."
+            ),
+        ]
+    ),
+    error=Sequence[
+        ErrorType.INVALID_CLASS,
+        ErrorType.INVALID_OBJECT,
+        ErrorType.VM_DEAD,
+    ],
 )
 
+
 ReferenceType = CommandSet(
-    name="VirtualMachine",
+    name="ReferenceType",
     id=2,
     commands=[
         Signature,
-        ClassLoader,
-        Modifiers,
-        Fields,
-        Methods,
-        GetValues,
-        SourceFile,
-        NestedTypes,
-        Status,
-        Interfaces,
-        ClassObject,
-        SourceDebugExtension,
-        SignatureWithGeneric,
-        FieldsWithGeneric,
-        MethodsWithGeneric,
-        Instances,
-        ClassFileVersion,
-        ConstantPool,
     ],
 )
