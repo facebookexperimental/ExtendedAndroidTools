38b3c9df9cc6391f8343e0973c699c4cf44eab60
Michael A. Wekesa <wekesa360@outlook.com>
2023-11-07 21:10:24 +0300

    update arraytype in AllClasses command

--- projects/jdwp/defs/command_sets/virtual_machine.py
+++ projects/jdwp/defs/command_sets/virtual_machine.py
@@ -2,25 +2,52 @@
 
 """JDWP Commands for ThreadReference Command Set."""
 
-from projects.jdwp.defs.schema import Command, Field, Struct, Type, Array, CommandSet
+from projects.jdwp.defs.schema import (
+    Command,
+    Field,
+    Struct,
+    Type,
+    Array,
+    CommandSet,
+    ArrayLength,
+    IntegralType,
+)
 from projects.jdwp.defs.constants import ErrorType
 
-from projects.jdwp.defs.schema import Command, Field, Struct, Type, Array
-from projects.jdwp.defs.constants import ErrorType
+
+__AllClasses_reply_classes = Field(
+    "classes",
+    ArrayLength(IntegralType.INT),
+    "Number of reference type that follow",
+)
+
+
+__AllClasses_reply_classesArray_element = Struct(
+    [
+        Field("refTypeTag", IntegralType.BYTE, "Kind of following reference type"),
+        Field("typeID", Type.REFERENCE_TYPE_ID, "Loaded reference type"),
+        Field(
+            "signature", Type.STRING, "The JNI signature of the loaded reference type"
+        ),
+        Field("status", IntegralType.INT, "The current class status"),
+    ]
+)
+
+__AllClasses_reply = Struct(
+    [
+        __AllClasses_reply_classes,
+        Field(
+            "classesArray",
+            Array(__AllClasses_reply_classesArray_element, __AllClasses_reply_classes),
+            "Array of reference type.",
+        ),
+    ]
+)
 
 AllClasses = Command(
     name="AllClasses",
     id=3,
-    out=Struct(
-        fields=[
-            Field("classes", Type.INT, "Number of reference types that follow."),
-            Field(
-                "referenceTypes",
-                Array(Type.BYTE, Type.INT),
-                "Array of reference types.",
-            ),
-        ]
-    ),
+    out=__AllClasses_reply,
     reply=None,
     error={
         ErrorType.INVALID_CLASS_LOADER,
