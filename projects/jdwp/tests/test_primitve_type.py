57fbd10cee4275a3c0689cbda03d27866082d5d5
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-11-15 18:56:39 +0000

    Changed testing framework to unittest

--- projects/jdwp/tests/test_primitve_type.py
+++ projects/jdwp/tests/test_primitve_type.py
@@ -1,11 +1,17 @@
-import pytest
+import unittest
 from projects.jdwp.codegen.new_type_generator import get_python_type
 from projects.jdwp.defs.schema import PrimitiveType
 
 
-@pytest.mark.parametrize("jdwp_type", PrimitiveType)
-def test_enum_member_mapping(jdwp_type):
-    result = get_python_type(jdwp_type)
-    assert isinstance(
-        result, str
-    ), f"Mapping for {jdwp_type} is missing or not a string"
+class TestEnumMemberMapping(unittest.TestCase):
+    def test_enum_member_mappings(self):
+        for jdwp_type in PrimitiveType:
+            with self.subTest(jdwp_type=jdwp_type):
+                result = get_python_type(jdwp_type)
+                self.assertIsInstance(
+                    result, str, f"Mapping for {jdwp_type} is missing or not a string"
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- projects/jdwp/tests/test_type_alias_definition.py
+++ projects/jdwp/tests/test_type_alias_definition.py
@@ -1,12 +1,19 @@
-import pytest
+import unittest
 from projects.jdwp.defs.schema import PrimitiveType
 from projects.jdwp.codegen.new_type_generator import get_type_alias_definition
 
 
-@pytest.mark.parametrize("jdwp_type", PrimitiveType)
-def test_type_alias_definition(jdwp_type):
-    expected_start = f"{jdwp_type.name.capitalize()}Type = NewType"
-    definition = get_type_alias_definition(jdwp_type)
-    assert definition.startswith(
-        expected_start
-    ), f"Definition for {jdwp_type} is incorrect"
+class TestTypeAliasDefinition(unittest.TestCase):
+    def test_type_alias_definitions(self):
+        for jdwp_type in PrimitiveType:
+            with self.subTest(jdwp_type=jdwp_type):
+                expected_start = f"{jdwp_type.name.capitalize()}Type = NewType"
+                definition = get_type_alias_definition(jdwp_type)
+                self.assertTrue(
+                    definition.startswith(expected_start),
+                    f"Definition for {jdwp_type} is incorrect",
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
