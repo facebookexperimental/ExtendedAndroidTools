380e5dfe9a836d91151a5106c75e1c380fbd5829
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-11-15 15:53:54 +0000

    Used absolute name format for newtype

--- projects/jdwp/codegen/new_type_generator.py
+++ projects/jdwp/codegen/new_type_generator.py
@@ -1,5 +1,6 @@
 """New type Generator."""
 from projects.jdwp.defs.schema import PrimitiveType
+import typing
 
 
 def get_python_type(jdwp_type: PrimitiveType) -> str:
@@ -17,7 +18,7 @@ def get_type_alias_definition(jdwp_type: PrimitiveType) -> str:
     """Return the type alias definition for a given JDWP type."""
     python_type = get_python_type(jdwp_type)
     new_type_name = f"{jdwp_type.name.capitalize()}Type"
-    return f"{new_type_name} = NewType('{new_type_name}', {python_type})"
+    return f"{new_type_name} = typing.NewType('{new_type_name}', {python_type})"
 
 
 def generate_new_types():
