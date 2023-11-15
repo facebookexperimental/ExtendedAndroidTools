315e3e9b8ab4120a3a051371cce27559b0b8e7de
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-11-15 19:20:50 +0000

    Changed to unittest

--- projects/jdwp/tests/test_dataclass_generator.py
+++ projects/jdwp/tests/test_dataclass_generator.py
@@ -1,3 +1,4 @@
+import unittest
 from projects.jdwp.codegen.dataclass_generator import (
     generate_dataclass_for_command,
     generate_dataclass_for_struct,
@@ -5,41 +6,47 @@ from projects.jdwp.codegen.dataclass_generator import (
 from projects.jdwp.defs.schema import Command, Field, Struct
 
 
-def test_generate_dataclass_for_struct():
-    # Create a mock Struct
-    mock_struct = Struct(
-        fields=[
-            Field(name="field1", type="int", description="An integer field"),
-            Field(name="field2", type="str", description="A string field"),
-        ]
-    )
+class TestDataclassGenerator(unittest.TestCase):
+    def test_generate_dataclass_for_struct(self):
+        # Create a mock Struct
+        mock_struct = Struct(
+            fields=[
+                Field(name="field1", type="int", description="An integer field"),
+                Field(name="field2", type="str", description="A string field"),
+            ]
+        )
 
-    expected_output = (
-        "@dataclasses.dataclass(frozen=True)\n"
-        "class MockStruct:\n"
-        "    field1: int\n"
-        "    field2: str\n"
-    )
+        expected_output = (
+            "@dataclasses.dataclass(frozen=True)\n"
+            "class MockStruct:\n"
+            "    field1: int\n"
+            "    field2: str\n"
+        )
 
-    assert generate_dataclass_for_struct(mock_struct, "MockStruct") == expected_output
+        self.assertEqual(
+            generate_dataclass_for_struct(mock_struct, "MockStruct"), expected_output
+        )
+
+    def test_generate_dataclass_for_command(self):
+        mock_command = Command(
+            name="TestCommand",
+            id=1,
+            out=Struct(fields=[Field(name="outField", type="int", description="")]),
+            reply=Struct(fields=[Field(name="replyField", type="str", description="")]),
+            error=set(),
+        )
+
+        expected_output = (
+            "@dataclasses.dataclass(frozen=True)\n"
+            "class TestCommandCommand:\n"
+            "    outField: int\n\n"
+            "@dataclasses.dataclass(frozen=True)\n"
+            "class TestCommandResponse:\n"
+            "    replyField: str\n"
+        )
+
+        self.assertEqual(generate_dataclass_for_command(mock_command), expected_output)
 
 
-def test_generate_dataclass_for_command():
-    mock_command = Command(
-        name="TestCommand",
-        id=1,
-        out=Struct(fields=[Field(name="outField", type="int", description="")]),
-        reply=Struct(fields=[Field(name="replyField", type="str", description="")]),
-        error=set(),
-    )
-
-    expected_output = (
-        "@dataclasses.dataclass(frozen=True)\n"
-        "class TestCommandCommand:\n"
-        "    outField: int\n\n"
-        "@dataclasses.dataclass(frozen=True)\n"
-        "class TestCommandResponse:\n"
-        "    replyField: str\n"
-    )
-
-    assert generate_dataclass_for_command(mock_command) == expected_output
+if __name__ == "__main__":
+    unittest.main()
