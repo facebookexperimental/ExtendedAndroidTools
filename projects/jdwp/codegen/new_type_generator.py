16396adb1eb47acd1972f9d3f3b3d4132b8f2f78
Christian Abrokwa <cabrokwa11@gmail.com>
2023-11-21 17:31:43 +0000

    Added newtype support for primitive type enum (#76)
    * Added newtype support for primitive type enum
    
    * Make newgen generator automatic
    
    * Changed type of jdwp_type and changed default type to int
    
    * Wrote test to validate primitive type mapping
    
    * Added test for type alias definition
    
    * Factored out function defining type alias.
    
    * Used absolute name format for newtype
    
    * Changed testing framework to unittest
    
    * Added dependency for jdwp project
    
    * Added int and string type
    
    * Changed the expected start
    
    * Updated test and removed int and byte from primitive types.
    
    * Removed int and boolean type from new type generator
    
    * doc: add copyright

---
+++ projects/jdwp/codegen/new_type_generator.py
@@ -0,0 +1,26 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+from projects.jdwp.defs.schema import PrimitiveType
+import typing
+
+
+def get_python_type(jdwp_type: PrimitiveType) -> str:
+    """Map JDWP type to Python type."""
+    mapping = {
+        PrimitiveType.STRING: "str",
+        PrimitiveType.BOOLEAN: "bool",
+    }
+    return mapping.get(jdwp_type, "int")
+
+
+def get_type_alias_definition(jdwp_type: PrimitiveType) -> str:
+    """Return the type alias definition for a given JDWP type."""
+    python_type = get_python_type(jdwp_type)
+    new_type_name = f"{jdwp_type.name.capitalize()}Type"
+    return f"{new_type_name} = typing.NewType('{new_type_name}', {python_type})"
+
+
+def generate_new_types():
+    for jdwp_type in PrimitiveType:
+        type_alias_definition = get_type_alias_definition(jdwp_type)
+        print(type_alias_definition)
