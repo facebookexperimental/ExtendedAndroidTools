17cfebe59d4b3dde0e80b5bc3bdf020434b40510
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-11-23 15:29:01 +0000

    Made input and output stream classes abstract

--- projects/jdwp/runtime/async_streams.py
+++ projects/jdwp/runtime/async_streams.py
@@ -1,44 +1,185 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
 import asyncio
+import abc
 
 
-class AsyncInputStream:
-    def __init__(self, reader: asyncio.StreamReader):
+class JDWPInputStreamBase(abc.ABC):
+    def __init__(self, reader: asyncio.StreamReader) -> None:
         self.reader = reader
 
-    async def read(self, n: int = -1) -> bytes:
-        """
-        Asynchronously read n bytes from the stream.
-        If n is -1 (default), read until EOF.
-        """
-        return await self.reader.read(n)
+    # Methods for OpaqueType
+    @abc.abstractmethod
+    async def read_boolean(self) -> bool:
+        pass
 
-    async def readexactly(self, n: int) -> bytes:
-        """
-        Asynchronously read exactly n bytes from the stream.
-        """
-        return await self.reader.readexactly(n)
+    @abc.abstractmethod
+    async def read_location(self) -> str:
+        pass
 
-    async def readline(self) -> bytes:
-        """
-        Asynchronously read one line, where “line” is a sequence of bytes ending with \n.
-        """
-        return await self.reader.readline()
+    @abc.abstractmethod
+    async def read_string(self) -> str:
+        pass
+
+    # Methods for IdType
+    @abc.abstractmethod
+    async def read_object_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_thread_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_thread_group_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_string_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_class_loader_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_class_object_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_array_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_reference_type_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_class_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_interface_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_array_type_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_method_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_field_id(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def read_frame_id(self) -> str:
+        pass
+
+    # Methods for IntegralType
+    @abc.abstractmethod
+    async def read_byte(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    async def read_int(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    async def read_long(
+        self,
+    ) -> int:
+        pass
 
 
-class AsyncOutputStream:
-    def __init__(self, writer: asyncio.StreamWriter):
+class JDWPOutputStreamBase(abc.ABC):
+    def __init__(self, writer: asyncio.StreamWriter) -> None:
         self.writer = writer
 
-    async def write(self, data: bytes) -> None:
-        """
-        Asynchronously write data to the stream.
-        """
-        self.writer.write(data)
-        await self.writer.drain()
+    # Methods for OpaqueType
+    @abc.abstractmethod
+    def write_boolean(self, value: bool) -> None:
+        pass
 
-    async def close(self):
-        """
-        Close the stream.
-        """
-        self.writer.close()
-        await self.writer.wait_closed()
+    @abc.abstractmethod
+    def write_location(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_string(self, value: str) -> None:
+        pass
+
+    # Methods for IdType
+    @abc.abstractmethod
+    def write_object_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_thread_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_thread_group_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_string_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_class_loader_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_class_object_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_array_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_reference_type_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_class_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_interface_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_array_type_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_method_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_field_id(self, value: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_frame_id(self, value: str) -> None:
+        pass
+
+    # Methods for IntegralType
+    @abc.abstractmethod
+    def write_byte(self, value: int) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_int(self, value: int) -> None:
+        pass
+
+    @abc.abstractmethod
+    def write_long(
+        self,
+        value: int,
+    ) -> None:
+        pass
