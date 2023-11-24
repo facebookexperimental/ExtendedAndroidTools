987b4c234a260f84ad1125dc372a184be4ed3ccd
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-11-24 16:27:57 +0000

    feat: built dataclass generator

--- projects/jdwp/codegen/dataclass_generator.py
+++ projects/jdwp/codegen/dataclass_generator.py
@@ -4,59 +4,82 @@ from projects.jdwp.defs.schema import (
     Array,
     ArrayLength,
     Command,
+    IdType,
     IntegralType,
-    PrimitiveType,
     Struct,
     TaggedUnion,
+    UnionTag,
 )
 
 
-def map_primitive_type(primitive_type):
-    match primitive_type:
-        case PrimitiveType.STRING:
-            return "str"
-        case PrimitiveType.BOOLEAN:
-            return "bool"
-        case PrimitiveType.REFERENCE_TYPE_ID:
-            return "ReferenceTypeId"
-        case PrimitiveType.CLASS_LOADER:
-            return "ClassLoaderId"
-        case PrimitiveType.FIELD_ID:
-            return "FieldId"
-        case PrimitiveType.METHOD_ID:
-            return "MethodId"
-        case PrimitiveType.VALUE:
-            return "Value"
-        case PrimitiveType.INTERFACE_ID:
-            return "InterfaceId"
-        case PrimitiveType.CLASS_OBJECT_ID:
-            return "ClassObjectId"
-        case PrimitiveType.TAGGED_OBJECT_ID:
-            return "TaggedObjectId"
-        case PrimitiveType.THREAD_ID:
-            return "ThreadId"
-        case PrimitiveType.THREAD_GROUP_ID:
-            return "ThreadGroupId"
-        case PrimitiveType.OBJECT_ID:
-            return "ObjectId"
-        case PrimitiveType.LOCATION:
-            return "Location"
+def format_enum_name(enum_value):
+    words = enum_value.name.split("_")
+    formatted_name = "".join(word.capitalize() for word in words)
+    return f"{formatted_name}Type"
+
+
+def map_id_type(id_type: IdType):
+    match id_type:
+        case IdType.OBJECT_ID:
+            return "ObjectIdType"
+        case IdType.THREAD_ID:
+            return "ThreadIdType"
+        case IdType.THREAD_GROUP_ID:
+            return "ThreadGroupIdType"
+        case IdType.STRING_ID:
+            return "StringIdType"
+        case IdType.CLASS_LOADER_ID:
+            return "ClassLoaderIdType"
+        case IdType.CLASS_OBJECT_ID:
+            return "ClassObjectIdType"
+        case IdType.ARRAY_ID:
+            return "ArrayIdType"
+        case IdType.REFERENCE_TYPE_ID:
+            return "ReferenceTypeIdType"
+        case IdType.CLASS_ID:
+            return "ClassIdType"
+        case IdType.INTERFACE_ID:
+            return "InterfaceIdType"
+        case IdType.ARRAY_TYPE_ID:
+            return "ArrayTypeIdType"
+        case IdType.METHOD_ID:
+            return "MethodIdType"
+        case IdType.FIELD_ID:
+            return "FieldIdType"
+        case IdType.FRAME_ID:
+            return "FrameIdType"
         case _:
-            return f"Unrecognized type: {primitive_type}"
+            return f"Unrecognized type: {id_type}"
 
 
-def get_python_type_for_field(field_type):
-    if isinstance(field_type, PrimitiveType):
-        return map_primitive_type(field_type)
-    elif isinstance(field_type, IntegralType) or isinstance(field_type, ArrayLength):
+def get_python_type_for_field(field_type, parent_struct_name="", field_name=""):
+    if isinstance(field_type, IdType):
+        return map_id_type(field_type)
+    elif (
+        isinstance(field_type, IntegralType)
+        or isinstance(field_type, ArrayLength)
+        or isinstance(field_type, UnionTag)
+    ):
         return "int"
     elif isinstance(field_type, Array):
-        element_type = get_python_type_for_field(field_type.element_type)
+        element_type = get_python_type_for_field(
+            field_type.element_type, parent_struct_name, field_name + "Element"
+        )
         return f"List[{element_type}]"
     elif isinstance(field_type, TaggedUnion):
-        pass
+        union_types = [
+            f"{parent_struct_name}{field_name}Case{format_enum_name(case)}"
+            for case in field_type.cases.keys()
+        ]
+        union_types_str = ", ".join(union_types)
+        return f"typing.Union[{union_types_str}]"
     elif isinstance(field_type, Struct):
-        return "NestedStruct"
+        nested_struct_name = (
+            f"{parent_struct_name}{field_name}"
+            if parent_struct_name and field_name
+            else "NestedStruct"
+        )
+        return nested_struct_name
     else:
         raise Exception(f"Unrecognized type: {field_type}")
 
@@ -79,16 +102,15 @@ def generate_dataclass_for_command(command: Command) -> str:
 
 
 def generate_dataclass_for_struct(struct: Struct, class_name: str) -> str:
-    """Generates a dataclass definition for a given struct."""
     fields_def = "\n".join(
-        f"    {field.name}: {get_python_type_for_field(field.type)}"
+        f"    {field.name}: {get_python_type_for_field(field.type, class_name, field.name)}"
         for field in struct.fields
     )
     class_def = dedent(
         f"""
-    @dataclasses.dataclass(frozen=True)
-    class {class_name}:
+        @dataclasses.dataclass(frozen=True)
+        class {class_name}:
 {fields_def}
-    """
+        """
     )
     return class_def
--- projects/jdwp/tests/test_dataclass_generator.py
+++ projects/jdwp/tests/test_dataclass_generator.py
@@ -1,51 +1,150 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+from enum import Enum
+from textwrap import dedent
 import unittest
-from projects.jdwp.codegen.dataclass_generator import (
-    generate_dataclass_for_command,
-    generate_dataclass_for_struct,
+from projects.jdwp.defs.schema import (
+    IdType,
+    IntegralType,
+    Array,
+    Struct,
+    Field,
+    Command,
+    TaggedUnion,
+    UnionTag,
 )
-from projects.jdwp.defs.schema import Command, Field, Struct
+from projects.jdwp.codegen.dataclass_generator import (
+    format_enum_name,
+    map_id_type,
+    get_python_type_for_field,
+    generate_dataclass_for_struct,
+    generate_dataclass_for_command,
+)
 
 
-class TestDataclassGenerator(unittest.TestCase):
-    def test_generate_dataclass_for_struct(self):
-        # Create a mock Struct
-        mock_struct = Struct(
-            fields=[
-                Field(name="field1", type="int", description="An integer field"),
-                Field(name="field2", type="str", description="A string field"),
-            ]
+class MockEnum(Enum):
+    OBJECT_ID = 1
+    THREAD_ID = 2
+    THREAD_GROUP_ID = 3
+
+
+class TestJDWPSchema(unittest.TestCase):
+    def test_format_enum_name(self):
+        self.assertEqual(format_enum_name(MockEnum.OBJECT_ID), "ObjectIdType")
+        self.assertEqual(format_enum_name(MockEnum.THREAD_ID), "ThreadIdType")
+        self.assertEqual(
+            format_enum_name(MockEnum.THREAD_GROUP_ID), "ThreadGroupIdType"
         )
 
-        expected_output = (
-            "@dataclasses.dataclass(frozen=True)\n"
-            "class MockStruct:\n"
-            "    field1: int\n"
-            "    field2: str\n"
+    def test_map_id_type(self):
+        self.assertEqual(map_id_type(IdType.OBJECT_ID), "ObjectIdType")
+        self.assertEqual(map_id_type(IdType.THREAD_ID), "ThreadIdType")
+        self.assertEqual(map_id_type(IdType.THREAD_GROUP_ID), "ThreadGroupIdType")
+
+    def test_id_type(self):
+        self.assertEqual(get_python_type_for_field(IdType.OBJECT_ID), "ObjectIdType")
+
+    def test_integral_type(self):
+        self.assertEqual(get_python_type_for_field(IntegralType.INT), "int")
+
+    def test_array_type(self):
+        array_type = Array(element_type=IdType.OBJECT_ID, length=1)
+        self.assertEqual(get_python_type_for_field(array_type), "List[ObjectIdType]")
+
+    def test_tagged_union_type(self):
+        union_tag = UnionTag(tag=IntegralType.INT, value=IdType.OBJECT_ID)
+        tagged_union = TaggedUnion(
+            tag=Field(name="tagField", type=union_tag, description=""),
+            cases={IdType.OBJECT_ID: Struct([])},
         )
 
         self.assertEqual(
-            generate_dataclass_for_struct(mock_struct, "MockStruct"), expected_output
+            get_python_type_for_field(tagged_union, "Parent", "Field"),
+            "typing.Union[ParentFieldCaseObjectIdType]",
+        )
+
+    def test_struct_type(self):
+        struct = Struct(fields=[])
+        self.assertEqual(
+            get_python_type_for_field(struct, "Parent", "Field"), "ParentField"
+        )
+
+    def test_simple_struct(self):
+        struct = Struct(
+            fields=[Field(name="simpleField", type=IdType.OBJECT_ID, description="")]
+        )
+        expected_class_def = dedent(
+            """
+            @dataclasses.dataclass(frozen=True)
+                class SimpleStruct:
+            simpleField: ObjectIdType
+            """
+        ).strip()
+        self.assertEqual(
+            generate_dataclass_for_struct(struct, "SimpleStruct").strip(),
+            expected_class_def,
+        )
+
+    def test_complex_struct(self):
+        struct = Struct(
+            fields=[
+                Field(name="idField", type=IdType.OBJECT_ID, description=""),
+                Field(name="intField", type=IntegralType.INT, description=""),
+                Field(
+                    name="arrayField",
+                    type=Array(element_type=IntegralType.INT, length=1),
+                    description="",
+                ),
+                Field(
+                    name="unionField",
+                    type=TaggedUnion(tag=None, cases={}),
+                    description="",
+                ),
+            ]
+        )
+        expected_class_def = dedent(
+            """
+            @dataclasses.dataclass(frozen=True)
+                class ComplexStruct:
+            idField: ObjectIdType
+            intField: int
+            arrayField: List[int]
+            unionField: typing.Union[]
+            """
+        ).strip()
+        self.assertEqual(
+            generate_dataclass_for_struct(struct, "ComplexStruct").strip(),
+            expected_class_def,
         )
 
     def test_generate_dataclass_for_command(self):
-        mock_command = Command(
-            name="TestCommand",
-            id=1,
-            out=Struct(fields=[Field(name="outField", type="int", description="")]),
-            reply=Struct(fields=[Field(name="replyField", type="str", description="")]),
-            error=set(),
+        out_struct = Struct(
+            fields=[Field(name="idField", type=IdType.OBJECT_ID, description="")]
+        )
+        reply_struct = Struct(
+            fields=[Field(name="status", type=IntegralType.INT, description="")]
         )
 
-        expected_output = (
-            "@dataclasses.dataclass(frozen=True)\n"
-            "class TestCommandCommand:\n"
-            "    outField: int\n\n"
-            "@dataclasses.dataclass(frozen=True)\n"
-            "class TestCommandResponse:\n"
-            "    replyField: str\n"
+        command = Command(
+            name="SampleCommand", id=1, out=out_struct, reply=reply_struct, error=set()
         )
 
-        self.assertEqual(generate_dataclass_for_command(mock_command), expected_output)
+        expected_output = dedent(
+            """
+            @dataclasses.dataclass(frozen=True)
+                class SampleCommandCommand:
+            idField: ObjectIdType
+
+            
+                @dataclasses.dataclass(frozen=True)
+                class SampleCommandResponse:
+            status: int
+            """
+        ).strip()
+
+        self.assertEqual(
+            generate_dataclass_for_command(command).strip(), expected_output
+        )
 
 
 if __name__ == "__main__":
