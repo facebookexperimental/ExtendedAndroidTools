da1671d6944b2d693b3a5b5d28f9ff32c2b24e48
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-11-29 14:29:55 +0000

    Feat: update functions and put it in a class.

--- projects/jdwp/codegen/dataclass_generator.py
+++ projects/jdwp/codegen/dataclass_generator.py
@@ -2,17 +2,55 @@
 
 from textwrap import dedent
 from projects.jdwp.codegen.types import python_type_for
+import typing
 
 from projects.jdwp.defs.schema import (
     Array,
-    ArrayLength,
-    Command,
-    IdType,
-    IntegralType,
+    Field,
     Struct,
     TaggedUnion,
-    UnionTag,
 )
+
+
+StructLink = typing.Tuple[Struct, Field, Struct]
+
+
+class StructGenerator:
+    def __init__(self, root: Struct, name: str):
+        self.__root = root
+        self.__struct_to_name = compute_struct_names(root, name)
+
+    def __get_python_type_for(self, struct: Struct, field: Field) -> str:
+        type = field.type
+        match type:
+            case Struct():
+                return self.__struct_to_name[type]
+            case Array():
+                return f"typing.List[{self.__struct_to_name[type.element_type]}]"
+            case TaggedUnion():
+                union_types = [
+                    self.__struct_to_name[case_struct]
+                    for case_struct in type.cases.values()
+                ]
+                union_types_str = ", ".join(union_types)
+                return f"typing.Union[{union_types_str}]"
+            case _:
+                return python_type_for(type)
+
+    def __generate_dataclass(self, struct: Struct) -> str:
+        name = self.__struct_to_name[struct]
+        fields_def = "\n".join(
+            f"    {field.name}: {self.__get_python_type_for(struct, field)}"
+            for field in struct.fields
+        )
+        class_def = f"@dataclasses.dataclass(frozen=True)\nclass {name}:\n{fields_def}"
+        return dedent(class_def)
+
+    def generate(self):
+        return [
+            self.__generate_dataclass(nested)
+            for _, _, nested in reversed(list(nested_structs(self.__root)))
+        ] + [self.__generate_dataclass(self.__root)]
 
 
 def format_enum_name(enum_value):
@@ -21,76 +59,35 @@ def format_enum_name(enum_value):
     return f"{formatted_name}Type"
 
 
-def get_python_type_for_field(field_type, parent_struct_name="", field_name=""):
-    if isinstance(field_type, IdType):
-        return python_type_for(field_type)
-    elif isinstance(field_type, IntegralType):
-        return "int"
-    elif isinstance(field_type, Array):
-        element_type = get_python_type_for_field(
-            field_type.element_type, parent_struct_name, field_name + "Element"
-        )
-        return f"List[{element_type}]"
-    elif isinstance(field_type, TaggedUnion):
-        union_types = [
-            f"{parent_struct_name}{field_name}Case{format_enum_name(case)}"
-            for case in field_type.cases.keys()
-        ]
-        union_types_str = ", ".join(union_types)
-        return f"typing.Union[{union_types_str}]"
-    elif isinstance(field_type, Struct):
-        nested_struct_name = (
-            f"{parent_struct_name}{field_name}"
-            if parent_struct_name and field_name
-            else "NestedStruct"
-        )
-        return nested_struct_name
-    elif isinstance(field_type, (ArrayLength, UnionTag)):
-        return None
-    else:
-        raise Exception(f"Unrecognized type: {field_type}")
+def nested_structs(root: Struct) -> typing.Generator[StructLink, None, None]:
+    for field in root.fields:
+        type = field.type
+        match type:
+            case Array():
+                yield root, field, type.element_type
+                yield from nested_structs(type.element_type)
+            case TaggedUnion():
+                for struct in type.cases.values():
+                    yield root, field, struct
+                    yield from nested_structs(struct)
+            case Struct():
+                yield root, field, type
+                yield from nested_structs(type)
 
 
-def generate_dataclass_for_command(command: Command) -> str:
-    """Generates a dataclass definition for a given command."""
-    out_class = (
-        generate_dataclass_for_struct(command.out, f"{command.name}Command")
-        if command.out
-        else ""
-    )
-
-    reply_class = (
-        generate_dataclass_for_struct(command.reply, f"{command.name}Response")
-        if command.reply
-        else ""
-    )
-
-    return out_class + "\n" + reply_class
-
-
-def generate_dataclass_for_struct(struct: Struct, class_name: str) -> str:
-    dataclass_definitions = []
-    fields_def = []
-
-    for field in struct.fields:
-        if isinstance(field.type, Struct):
-            nested_class_name = f"{class_name}{field.name.capitalize()}"
-            nested_class_def = generate_dataclass_for_struct(
-                field.type, nested_class_name
-            )
-            dataclass_definitions.append(nested_class_def)
-            field_type = nested_class_name
-        else:
-            field_type = get_python_type_for_field(field.type, class_name, field.name)
-
-        fields_def.append(f"    {field.name}: {field_type}")
-
-    class_def = (
-        "@dataclasses.dataclass(frozen=True)\n"
-        + f"class {class_name}:\n"
-        + "\n".join(fields_def)
-    )
-
-    dataclass_definitions.append(dedent(class_def))
-
-    return "\n\n".join(dataclass_definitions)
+def compute_struct_names(root: Struct, name: str) -> typing.Mapping[Struct, str]:
+    names = {root: name}
+    for parent, field, nested in nested_structs(root):
+        type = field.type
+        match type:
+            case Struct():
+                names[nested] = f"{names[parent]}{field.name.capitalize()}"
+            case Array():
+                names[nested] = f"{names[parent]}{field.name.capitalize()}Element"
+            case TaggedUnion():
+                for case_value, case_struct in field.type.cases.items():
+                    case_name = format_enum_name(case_value)
+                    names[
+                        case_struct
+                    ] = f"{names[parent]}{field.name.capitalize()}Case{case_name}"
+    return names
