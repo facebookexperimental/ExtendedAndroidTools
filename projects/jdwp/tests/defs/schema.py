babe5a87bed5a1958c24997985d90e8eed0ce92c
Michal Gregorczyk <michalgr@live.com>
2023-12-05 10:14:47 +0100

    [jdwp] make jdwp schema immutable and hashable

--- projects/jdwp/tests/defs/schema.py
+++ projects/jdwp/tests/defs/schema.py
@@ -7,9 +7,13 @@ class SchemaTests(unittest.TestCase):
     def test_schema_can_be_imported(self):
         from projects.jdwp.defs.schema import OpaqueType
 
-    def test_command_sets_can_be_imported(self):
+    def test_command_sets_are_hashable(self):
         from projects.jdwp.defs.command_sets import (
             event_request,
             reference_type,
             virtual_machine,
         )
+
+        hash(event_request.EventRequest)
+        hash(reference_type.ReferenceType)
+        hash(event_request.EventRequest)
