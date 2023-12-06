ad008aa0e73ad8882784a305315b7e647e9c5d29
Michal Gregorczyk <michalgr@live.com>
2023-12-06 17:52:34 +0100

    [jdwp] add tests importing components of the runtime library

--- projects/jdwp/tests/runtime/structs.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-
-import unittest
-
-
-class GeneratedStructsTest(unittest.TestCase):
-    def test_structs_can_be_imported(self):
-        from projects.jdwp.runtime.structs import IDSizesReply
