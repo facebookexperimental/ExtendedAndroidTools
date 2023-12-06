e63a28eaf96cbd2ecfb207106607b9af9ae0886f
Michal Gregorczyk <michalgr@live.com>
2023-12-06 12:58:18 +0100

    [jdwp] generate projects.jdwp.runtime.structs

---
+++ projects/jdwp/tests/runtime/structs.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+import unittest
+
+
+class GeneratedStructsTest(unittest.TestCase):
+    def test_structs_can_be_imported(self):
+        from projects.jdwp.runtime.structs import IDSizesReply
