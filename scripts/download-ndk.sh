8c7b652cfc27fa7f0f1f812f9d2311b265fabd94
Michal Gregorczyk <michalgr@live.com>
2024-09-11 18:53:00 +0200

    update NDK to r27b

--- scripts/download-ndk.sh
+++ scripts/download-ndk.sh
@@ -2,12 +2,12 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 dest="${1:-$(pwd)}"
-TMP_NDK="/tmp/android-ndk-r23b-linux.zip"
+TMP_NDK="/tmp/android-ndk-r27b-linux.zip"
 
 echo "downloading ndk to ${dest}..."
 
-curl -s -o "${TMP_NDK}" https://dl.google.com/android/repository/android-ndk-r23b-linux.zip
+curl -s -o "${TMP_NDK}" https://dl.google.com/android/repository/android-ndk-r27b-linux.zip
 unzip -q "${TMP_NDK}" -d "${dest}"
-rm /tmp/android-ndk-r23b-linux.zip
+rm /tmp/android-ndk-r27b-linux.zip
 
 echo "done"
