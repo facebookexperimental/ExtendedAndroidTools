30e28030f5ea63d4cd2cd58c6610894bc7ee01c2
Michal Gregorczyk <michalgr@live.com>
2024-09-11 18:57:36 +0200

    update bpftrace

--- projects/bpftrace/build.mk
+++ projects/bpftrace/build.mk
@@ -43,7 +43,7 @@ $(STRIP_THUNK): projects/bpftrace/strip-thunk | $(HOST_OUT_DIR)
 	@sed -e "s+<STRIP_PATH>+$(ANDROID_TOOLCHAIN_STRIP_PATH)+g" $< > $@
 	chmod +x $@
 
-BPFTRACE_COMMIT = v0.19.1
+BPFTRACE_COMMIT = a77b8061f9291793e15c54a19a2202643d900387
 BPFTRACE_REPO = https://github.com/iovisor/bpftrace.git/
 projects/bpftrace/sources:
 	git clone $(BPFTRACE_REPO) $@
