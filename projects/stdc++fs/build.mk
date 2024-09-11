9eb5ac33adf6d7e71df262ce79db3335c4016d2c
Michal Gregorczyk <michalgr@live.com>
2024-09-11 18:54:09 +0200

    fix compilation of c++fs thunk

--- projects/stdc++fs/build.mk
+++ projects/stdc++fs/build.mk
@@ -3,7 +3,7 @@
 stdc++fs: $(ANDROID_BUILD_DIR)/stdc++fs.done
 
 $(ANDROID_BUILD_DIR)/stdc++fs.done: $(ANDROID_BUILD_DIR) $(ANDROID_BUILD_DIR)/stdc++fs
-	$(ANDROID_TOOLCHAIN_PATH)/clang++ -c -std=c++17 projects/stdc++fs/thunks.cpp -o $(ANDROID_BUILD_DIR)/thunks.o
+	$(ANDROID_TOOLCHAIN_PATH)/clang++ -target $(ANDROID_TRIPLE) -c -std=c++17 projects/stdc++fs/thunks.cpp -o $(ANDROID_BUILD_DIR)/thunks.o
 	$(ANDROID_TOOLCHAIN_PATH)/llvm-ar rc $(ANDROID_OUT_DIR)/lib/libstdc++fs.a $(ANDROID_BUILD_DIR)/thunks.o
 	touch $@
 
