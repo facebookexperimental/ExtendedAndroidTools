e14dc9e0ad7291158f5264a4274f02a7d983c48c
Sim Sun <simsun@fb.com>
2025-05-15 14:21:36 -0700

    Use github mirrored gnulib repo
    I observe the github action jobs are flakey due to the server of `git.savannah.gnu.org` is not stable. 
    ```
    git clone https://git.savannah.gnu.org/git/gnulib.git projects/gnulib/sources
    Cloning into 'projects/gnulib/sources'...
    fatal: unable to access 'https://git.savannah.gnu.org/git/gnulib.git/': The requested URL returned error: 502
    make: *** [projects/gnulib/build.mk:12: projects/gnulib/sources] Error 128
    ```
    
    This commit tried to use the github mirror repo instead.

--- projects/gnulib/build.mk
+++ projects/gnulib/build.mk
@@ -7,7 +7,7 @@ $(GNULIB_ANDROID):
 	false
 
 GNULIB_COMMIT_HASH = 044bf893acee0a55b22b4be0ede0e3ce010c480a
-GNULIB_REPO = https://git.savannah.gnu.org/git/gnulib.git
+GNULIB_REPO = https://github.com/simpleton/gnulib-mirror
 projects/gnulib/sources:
 	git clone $(GNULIB_REPO) $@
 	cd $@ && git checkout $(GNULIB_COMMIT_HASH)
