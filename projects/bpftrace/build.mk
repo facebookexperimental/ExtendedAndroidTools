465ad6011b15b0fe59a58d1de7c1ebcb0444bf4c
Sim Sun <simsun@fb.com>
2025-06-09 10:55:56 -0700

    bpftrace: Fix __data_loc reads

--- projects/bpftrace/build.mk
+++ projects/bpftrace/build.mk
@@ -49,4 +49,4 @@ projects/bpftrace/sources:
 	git clone $(BPFTRACE_REPO) $@ && \
 	cd $@ && \
 	git checkout $(BPFTRACE_COMMIT) && \
-	git cherry-pick 4b5cbe120f581846d5397fa4b2a0cbd34e15c77a --no-commit
+	git cherry-pick 4b5cbe120f581846d5397fa4b2a0cbd34e15c77a 665b754a5800d80d44a95699a6d5ec545a7b7065 --no-commit -X theirs
